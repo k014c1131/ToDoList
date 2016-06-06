36c81d2d7f3e75e7d2320b68adbb146c3682ed62
吉田 <k014c1131@it-neec.jp>
2016-06-06 11:08:38 +0900

    first commit

--- db.sh
+++ db.sh
@@ -1,11 +1,8 @@
 
 #!/bin/bash
 
-//データベースを削除
-mysql -uroot -proot -e "drop datebase todolist;"
+mysql -uroot  -e "drop database todolist;"
 
-//データベースtodolistの作成
-mysql -uroot -proot -e "create datebase todolist character set UTF8;"
+mysql -uroot  -e "create database todolist character set UTF8;"
 
-//テーブルlistの作成
-mysql -uroot -proot todolist-e "create tablelist id MEDIUMINT NOT NULL AUTO_INCREMENT , chk(1) NOT NULL DEFAULT '0' , item varchar(256) , PRIMARY KEY(id));"
+mysql -uroot  todolist -e "create table list (id MEDIUMINT NOT NULL AUTO_INCREMENT  , item varchar(256) , PRIMARY KEY(id));"
